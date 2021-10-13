a473180ae232a133576a494481fce754d1029f34
William Guillermin <guillermin.w@gmail.com>
2021-10-13 11:23:52 +0200

    fix v1

--- app/src/main/java/com/illiouchine/timercompose/ui/composable/chono/Marker.kt
+++ app/src/main/java/com/illiouchine/timercompose/ui/composable/chono/Marker.kt
@@ -25,6 +25,10 @@ internal fun CircleMarkers(
         Marker(
             angle = i * (360 / nbMarker),
             active = i < seconds * (nbMarker / totalSeconds)
+
+
+
+        
         )
     }
 }
